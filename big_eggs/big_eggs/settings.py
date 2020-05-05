b3c0fddd5cc0eb40989ae3ff8db07cf395672c7c
Bogumil Schube <bogumil@schube.info>
2020-05-05 15:51:46 +0200

    mail config

--- big_eggs/big_eggs/settings.py
+++ big_eggs/big_eggs/settings.py
@@ -20,6 +20,8 @@ env = environ.Env(
 )
 environ.Env.read_env()
 
+ADMINS = [x.split(':') for x in env.list('ADMINS')]
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
