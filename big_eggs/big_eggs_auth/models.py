86aa6af28bb96abb68425e0452fd94779842f6ed
Bogumil Schube <bogumil@schube.info>
2020-05-19 15:38:43 +0200

    create new tenants for new user

--- big_eggs/big_eggs_auth/models.py
+++ big_eggs/big_eggs_auth/models.py
@@ -3,9 +3,20 @@ import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from tenants.models import Tenant
+
 
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     def __str__(self):
         return self.email
+
+    def save(self, *args, **kwargs):
+        new_user = hasattr(self, "id")
+        super().save(*args, **kwargs)
+
+        if new_user:
+            t = Tenant()
+            t.save()
+            t.users.add(self)
