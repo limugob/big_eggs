2cbed2019a93e7af912806b1ab99870b290e7281
Bogumil Schube <bogumil@schube.info>
2020-08-31 12:24:24 +0200

    admin: show sessions

--- big_eggs/core/admin.py
+++ big_eggs/core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.sessions.models import Session
 
-# Register your models here.
+
+class SessionsAdmin(admin.ModelAdmin):
+    list_display = (
+        "session_key",
+        "expire_date",
+    )
+    readonly_fields = ("get_decoded",)
+
+    fields = (
+        "session_key",
+        "expire_date",
+        "get_decoded",
+    )
+
+
+admin.site.register(Session, SessionsAdmin)
