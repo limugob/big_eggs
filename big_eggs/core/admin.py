4492af090029b14ace876da16e7f4876e2871079
Bogumil Schube <bogumil@schube.info>
2020-09-18 12:57:55 +0200

    admin: show user from session

--- big_eggs/core/admin.py
+++ big_eggs/core/admin.py
@@ -1,19 +1,23 @@
 from django.contrib import admin
 from django.contrib.sessions.models import Session
 
+from big_eggs_auth.models import User
+
 
 class SessionsAdmin(admin.ModelAdmin):
-    list_display = (
-        "session_key",
-        "expire_date",
-    )
-    readonly_fields = ("get_decoded",)
+    list_display = ("session_key", "expire_date", "get_user")
+    readonly_fields = ("get_decoded", "get_user")
 
     fields = (
         "session_key",
         "expire_date",
         "get_decoded",
+        "get_user",
     )
+
+    def get_user(self, object):
+        if "_auth_user_id" in object.get_decoded():
+            return User.objects.get(id=object.get_decoded()["_auth_user_id"])
 
 
 admin.site.register(Session, SessionsAdmin)
